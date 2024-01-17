cd794e5325c1d78abf4c48449ddb0caa3532949c
Jared Hanson <jaredhanson@gmail.com>
2024-01-17 11:14:11 -0800

    Clarify tests.

--- test/handlers/prompt.test.js
+++ test/handlers/prompt.test.js
@@ -2,45 +2,51 @@
 
 var expect = require('chai').expect;
 var chai = require('chai');
+var $require = require('proxyquire');
 var sinon = require('sinon');
 var factory = require('../../com/handlers/prompt');
 
 
-describe('signup/http/handlers/prompt', function() {
+describe('signup/handlers/prompt', function() {
   
-  var handler;
-  
-  before(function() {
-    function state() {
-      return function(req, res, next) {
-        next();
-      };
-    }
+  it('should create handler', function() {
+    var csurfSpy = sinon.spy();
+    var flowstateSpy = sinon.spy();
+    var factory = $require('../../com/handlers/prompt', {
+      'csurf': csurfSpy,
+      'flowstate': flowstateSpy
+    });
     
-    //var stateSpy;
+    var store = new Object();
+    var handler = factory(store);
     
-    //stateSpy = sinon.spy(state);
-    
-    handler = factory(undefined);
-    
-    //expect(stateSpy).to.be.calledOnce;
+    expect(handler).to.be.an('array');
+    expect(csurfSpy).to.be.calledOnce;
+    expect(csurfSpy).to.be.calledBefore(flowstateSpy);
+    expect(flowstateSpy).to.be.calledOnce;
+    expect(flowstateSpy).to.be.calledWith({ store: store });
   });
   
-  it('should prompt to create account', function(done) {
+  describe('handler', function() {
     
-    chai.express.use(handler)
-      .request(function(req, res) {
-        req.connection = {};
-        req.session = {};
-      })
-      .finish(function() {
-        expect(this).to.have.status(200);
-        expect(this).to.render('account/signup');
-        expect(this).to.include.locals([ 'csrfToken' ]);
-        done();
-      })
-      .listen();
+    it('should render', function(done) {
+      var store = new Object();
+      var handler = factory(store);
     
-  }); // should prompt to create account
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.session = {};
+          req.connection = {};
+        })
+        .finish(function() {
+          expect(this).to.have.status(200);
+          expect(this).to.render('account/signup');
+          expect(this).to.include.locals([ 'csrfToken' ]);
+          done();
+        })
+        .listen();
+    }); // should render
+    
+  }); // handler
   
 });
